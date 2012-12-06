e23a178ec02da197debea22ce82e3a14d75183da
Brent <brenthaas+git@gmail.com>
2012-12-06 00:32:19 -0800

    rearranged spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe User do
+	let(:user) { User.new(first_name: "John", last_name: "Doe", email: "valid@email.com") }
+	subject { user }
+
+	it_validates "presence of", :user, :first_name
+	it_validates "presence of", :user, :last_name
+
 	describe "factory" do
 		it "creates a valid factory" do
 			user = FactoryGirl.create(:user)	
@@ -8,13 +14,7 @@ describe User do
 		end
 	end
 
-	let!(:user) { User.new(first_name: "John", last_name: "Doe", email: "valid@email.com") }
-	subject { user }
-
-	it_validates "presence of", :user, :first_name
-	it_validates "presence of", :user, :last_name
-
-	specify "name is first and last name" do
+	it "combines first and last name into name" do
 		user.name.should == user.first_name + " " + user.last_name
 	end
 end
