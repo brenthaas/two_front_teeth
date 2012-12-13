441f023ce09ff17549b63393a9766669c5e1e422
Brent <brenthaas+git@gmail.com>
2012-12-12 20:59:06 -0800

    Added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name, :last_name, :email
   validates_uniqueness_of :email
+  validates_email_format_of :email
 
   def name
     first_name + " " + last_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { User.new(first_name: "John", last_name: "Doe", email: "valid@email.com") }
+  let(:user) do 
+    User.create!(first_name: "John", last_name: "Doe", email: "valid@email.com")
+  end
   subject { user }
 
   describe "validations" do
@@ -11,6 +13,29 @@ describe User do
     describe "email" do
       it { should validate_presence_of :email }
       it { should validate_uniqueness_of :email }
+      describe "when email format" do
+        let(:invalid_emails) { %w[user@foo,com 
+                                  user_at_foo.org 
+                                  example.user@foo.
+                                  foo@bar_baz.com 
+                                  foo@bar+baz.com] }
+
+        let(:valid_emails) { %w[user@foo.COM 
+                                A_US-ER@f.b.org 
+                                frst.lst@foo.jp 
+                                a+b@baz.cn] }
+
+        it "is invalid" do
+          invalid_emails.each do |invalid_email| 
+            should_not allow_value(invalid_email).for(:email)
+          end
+        end 
+        it "is valid" do 
+          valid_emails.each do |valid_email|
+            should allow_value(valid_email).for(:email)
+          end
+        end
+      end
     end
   end
 
