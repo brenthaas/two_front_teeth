2a8f7355e6e8d37c74a76e9091171ecdcb1c0e75
Brent <brenthaas+git@gmail.com>
2012-12-12 23:14:11 -0800

    fixed whitespace

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,31 +9,26 @@ describe User do
   describe "validations" do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
-    
-    describe "email" do
-      it { should validate_presence_of :email }
-      it { should validate_uniqueness_of :email }
-      describe "when email format" do
-        let(:invalid_emails) { %w[user@foo,com 
-                                  user_at_foo.org 
-                                  example.user@foo.
-                                  foo@bar_baz.com 
-                                  foo@bar+baz.com] }
-
-        let(:valid_emails) { %w[user@foo.COM 
-                                A_US-ER@f.b.org 
-                                frst.lst@foo.jp 
-                                a+b@baz.cn] }
-
-        it "is invalid" do
-          invalid_emails.each do |invalid_email| 
-            should_not allow_value(invalid_email).for(:email)
-          end
-        end 
-        it "is valid" do 
-          valid_emails.each do |valid_email|
-            should allow_value(valid_email).for(:email)
-          end
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    describe "when email format" do
+      let(:invalid_emails) { %w[user@foo,com 
+                                user_at_foo.org 
+                                example.user@foo.
+                                foo@bar_baz.com 
+                                foo@bar+baz.com] }
+      let(:valid_emails) { %w[user@foo.COM 
+                              A_US-ER@f.b.org 
+                              frst.lst@foo.jp 
+                              a+b@baz.cn] }
+      it "is invalid" do
+        invalid_emails.each do |invalid_email| 
+          should_not allow_value(invalid_email).for(:email)
+        end
+      end 
+      it "is valid" do 
+        valid_emails.each do |valid_email|
+          should allow_value(valid_email).for(:email)
         end
       end
     end
